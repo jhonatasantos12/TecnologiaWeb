7ee5f6c5cc6070a615cee1a3f0b156194400540a
Jhonatas Santos <37161452+jhonatasantos12@users.noreply.github.com>
2022-04-12 18:40:27 -0300

    correcao

--- docker/app.py
+++ docker/app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
 app.config['MYSQL_DATABASE_DB'] = 'teste'
-app.config['MYSQL_DATABASE_HOST'] = '172.17.0.7'
-#app.config['MYSQL_DATABASE_HOST'] = '172.17.0.7'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+#app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
 
