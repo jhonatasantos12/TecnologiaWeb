c7b9e9cf90769b10edd0cb25a0dc771d9e531df1
Jhonatas Santos <37161452+jhonatasantos12@users.noreply.github.com>
2022-04-12 18:39:55 -0300

    Docker

---
+++ docker/app2.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
+#from werkzeug import generate_password_hash, check_password_hash
+
+mysql = MySQL()
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+#app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+
+@app.route('/list',methods=['POST','GET'])
+def signUp():
+    try:
+            
+            
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = _password
+            cursor.execute ('select user_name from tbl_user') 
+            data = cursor.fetchall()
+            print(data[0]);
+            for x in range(len(data)):
+                print(data[x])
+
+            conn.commit()
+            return render_template('signup2.html', datas=data)
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
